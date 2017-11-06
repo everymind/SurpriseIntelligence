abdbfaa2e6b09a183a5c08d07f7baa64606ceda2
glopesdev <glopesdev@users.noreply.github.com>
2017-11-06 11:07:21 +0000

    Updated watchdog script to generate log files

--- Workflows/Exhibit/Watchdog.cmd
+++ Workflows/Exhibit/Watchdog.cmd
@@ -1,4 +1,5 @@
 ..\..\Bonsai\Bonsai64.exe Warmup.bonsai --start --noeditor
 :while1
-..\..\Bonsai\Bonsai64.exe Exhibit.bonsai --start
+..\..\Bonsai\Bonsai64.exe Exhibit.bonsai --start --noeditor >> Log.txt 2>&1
+(echo %DATE% %TIME%) >> Log.txt
 goto :while1
